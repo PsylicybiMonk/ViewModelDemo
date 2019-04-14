a6b10814487b7aaf7084af2d69ccc1274e3ab975
Jeremy <JeremyFrancom@icloud.com>
2019-04-14 17:44:04 -0600

    Initial Commit of ViewModelDemo, an exercise out of New book android studio 3.2 dev essentials

---
+++ app/src/main/java/ebookfrenzy/com/ui/main/MainViewModel.java
@@ -0,0 +1,19 @@
+package ebookfrenzy.com.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    private static final Float usd_to_eu_rate = 0.74F;
+    private String dollarText = "";
+    private Float result = 0F;
+
+    public void setAmount(String value) {
+        this.dollarText = value;
+        result = Float.valueOf(dollarText)*usd_to_eu_rate;
+    }
+
+    public Float getResult()
+    {
+        return result;
+    }
+}
